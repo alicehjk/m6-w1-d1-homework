c6b71f891c708aaf19fd0d72087f60b9aa34905c
Alice Hyejin Kim <hyejik5@uci.edu>
2025-10-01 18:53:27 -0700

    update?

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import InventoryList from './InventoryList';
-import InventoryEdit from './InventoryEdit';
-import './App.css';
+import React, { Component } from "react";
+import Home from "./Home"
+import { Router, Routes, Route } from "react-router-dom";
+import InventoryList from "./InventoryList"
+import InventoryEdit from "./InventoryEdit"
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/inventories" component={InventoryList} />
-          <Route path="/inventories/:id" component={InventoryEdit} />
-        </Switch>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/inventories' exact element={<InventoryList />} />
+          <Route path='/inventories/:id' element={<InventoryEdit />} />
+        </Routes>
       </Router>
     );
   }
